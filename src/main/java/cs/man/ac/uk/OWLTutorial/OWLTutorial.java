718562c74f559c8fa0d8a01b450799272a368496
nogradiadam <nogradiadam@gmail.com>
2014-05-17 19:43:21 +0100

    adding stuff

---
+++ src/main/java/cs/man/ac/uk/OWLTutorial/OWLTutorial.java
@@ -0,0 +1,21 @@
+package cs.man.ac.uk.OWLTutorial;
+
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+
+public class OWLTutorial {
+
+	public static void main (String[] args) {
+		OWLOntologyManager man = OWLManager.createOWLOntologyManager();
+		IRI ontologyIRI = IRI.create("http://www.geneontology.org/ontology/obo_format_1_2/gene_ontology_ext.obo");
+		try {
+			OWLOntology ontology = man.loadOntology(ontologyIRI);
+			System.out.println(ontology.getLogicalAxiomCount());
+		} catch (OWLOntologyCreationException e) {
+			e.printStackTrace();
+		}
+	}
+}
