4ae47e49c0787ac1a67f4b65d254d25b2810496a
nogradiadam <nogradiadam@gmail.com>
2014-06-10 16:34:55 +0100

    Printing non-obsolete classes with definitions

--- src/main/java/cs/man/ac/uk/OWLTutorial/OWLHandler.java
+++ src/main/java/cs/man/ac/uk/OWLTutorial/OWLHandler.java
@@ -1,51 +1,49 @@
 package cs.man.ac.uk.OWLTutorial;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLAnnotation;
+import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLAnnotationProperty;
+import org.semanticweb.owlapi.model.OWLAnnotationValue;
+import org.semanticweb.owlapi.model.OWLAnonymousIndividual;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLClassExpression;
 import org.semanticweb.owlapi.model.OWLDataFactory;
-import org.semanticweb.owlapi.model.OWLLiteral;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
-import org.semanticweb.owlapi.vocab.OWLRDFVocabulary;
+
+import uk.ac.manchester.cs.owl.owlapi.OWLAnonymousIndividualImpl;
 
 public class OWLHandler {
 
 	private final String OBSOLETE_CLASS_URI = "<http://www.geneontology.org/formats/oboInOwl#ObsoleteClass>";
 	private OWLOntology ont;
 	private OWLDataFactory df;
-	private PrintWriter writer = null;
 
-	public void loadOntology (String ontURL) {
+	public void loadOntology (File ontURI) {
 		OWLOntologyManager man = OWLManager.createOWLOntologyManager();
 		try {
-			ont = man.loadOntologyFromOntologyDocument(IRI.create(ontURL));
+			ont = man.loadOntologyFromOntologyDocument(ontURI);
 		} catch (OWLOntologyCreationException e) {
 			e.printStackTrace();
 		}
-		System.out.println("Loaded: " + ont.getOntologyID());
-
 		df = man.getOWLDataFactory();
+		System.out.println("Loaded: " + ont.getOntologyID());
 	}
 
-	public void filterObsoleteClasses() {
-		try {
-			writer = new PrintWriter (new FileWriter("output_no_obsolete.txt"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public List<OWLClass> getNonObsoleteClasses() {
 
+		List<OWLClass> nonObsoleteList = new ArrayList <OWLClass> ();
 		boolean isObsoleteClass = false;
-		OWLAnnotationProperty label = df
-				.getOWLAnnotationProperty(OWLRDFVocabulary.RDFS_LABEL.getIRI());
 
 		for (OWLClass cls : ont.getClassesInSignature()) {
 			if (!(cls.getSuperClasses(ont).isEmpty())) {
@@ -55,18 +53,41 @@ public class OWLHandler {
 					}
 				}
 			}
-
 			if (!isObsoleteClass) {
-				for (OWLAnnotation annotation : cls.getAnnotations(ont, label)) {
-					if (annotation.getValue() instanceof OWLLiteral) {
-						OWLLiteral val = (OWLLiteral) annotation.getValue();
-						writer.println(val.getLiteral());
-					}
-				}
+				nonObsoleteList.add(cls);
 			}
+			
 			isObsoleteClass = false;
 		}
 
-		writer.close();
+		return nonObsoleteList;
+	}
+
+	public Map<OWLClass, String> getClassesWithDefinitions (List<OWLClass> classes){
+		OWLAnnotationProperty hasDefinition = df.getOWLAnnotationProperty(IRI.create("http://www.geneontology.org/formats/oboInOwl#hasDefinition"));
+		OWLAnnotationProperty rdfsLabel = df.getRDFSLabel();
+		Map<OWLClass, String> classesWithDefs = new TreeMap <OWLClass, String> ();
+
+		for(OWLClass cl:classes) {
+			Set<OWLAnnotation> annos = cl.getAnnotations(ont, hasDefinition);
+			for(OWLAnnotation anno:annos) {
+				OWLAnnotationValue value = anno.getValue();
+				if(value instanceof OWLAnonymousIndividualImpl) {
+					OWLAnonymousIndividual i = (OWLAnonymousIndividualImpl)value;
+					//					for(OWLClassExpression c:i.getTypes(go)) {
+					//						System.out.println(c);
+					//					}
+					for(OWLAnnotationAssertionAxiom ax:ont.getAnnotationAssertionAxioms(i)) {
+						if(ax.getProperty().equals(rdfsLabel)) {
+
+							OWLAnnotationValue av = ax.getValue();
+//							System.out.println(av);
+							classesWithDefs.put(cl, av.toString());
+						}
+					}
+				}
+			}
+		}
+		return classesWithDefs;
 	}
 }
--- src/main/java/cs/man/ac/uk/OWLTutorial/OWLTutorial.java
+++ src/main/java/cs/man/ac/uk/OWLTutorial/OWLTutorial.java
@@ -1,10 +1,35 @@
 package cs.man.ac.uk.OWLTutorial;
 
-public class OWLTutorial {
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
 
-	public static void main (String[] args) {		
+import org.semanticweb.owlapi.model.OWLClass;
+
+public class OWLTutorial {
+	
+	private static PrintWriter writer = null;
+	
+	public static void main (String[] args) {
+		try {
+			writer = new PrintWriter (new FileWriter("classes_with_definitions.txt"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		File goont = new File("/Users/AdamNogradi/Documents/workspace/OWLTutorial/go.owl");
 		OWLHandler handler = new OWLHandler();
-		handler.loadOntology("file:/Users/AdamNogradi/Documents/workspace/OWLTutorial/goOne.owl");
-		handler.filterObsoleteClasses();
+		handler.loadOntology(goont);
+		List<OWLClass> nonObsoleteClasses = handler.getNonObsoleteClasses();
+		Map<OWLClass, String> classesWithDefinitions =  handler.getClassesWithDefinitions(nonObsoleteClasses);
+		
+		for (Map.Entry<OWLClass, String> entry : classesWithDefinitions.entrySet()) {
+			writer.println("Class: " + entry.getKey() + "    Definition: " + entry.getValue());
+		}
+		
+		writer.close();
 	}
 }
